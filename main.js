e72b98f8c7e6be1aead2fbad43a7415c49af31da
lkarine <141235835+lkarine@users.noreply.github.com>
2025-04-14 10:15:52 -0300

    ff

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-14T00:00:00");
+const tempoObjetivo2 = new Date("2025-3-14T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-20T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-31T00:00:00");
 
